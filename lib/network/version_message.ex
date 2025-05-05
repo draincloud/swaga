18bee8bda7666f4bdc8a21f39a448b988a5382d2
Nikita Pozniakov <nikita.pozniakov@bitvavo.com>
2025-05-05 20:18:21 +0200

    feat: version message

--- lib/network/version_message.ex
+++ lib/network/version_message.ex
@@ -1,3 +1,5 @@
+require Logger
+
 defmodule VersionMessage do
   @enforce_keys [
     :version,
@@ -25,7 +27,7 @@ defmodule VersionMessage do
             sender_ip: <<0x00, 0x00, 0x00, 0x00>>,
             sender_port: 8333,
             nonce: nil,
-            user_agent: "emochka",
+            user_agent: "/programmingbitcoin:0.1/",
             latest_block: 0,
             relay: false
 
@@ -40,7 +42,7 @@ defmodule VersionMessage do
         sender_ip \\ <<0x00, 0x00, 0x00, 0x00>>,
         sender_port \\ 8333,
         nonce \\ nil,
-        user_agent \\ "emochka",
+        user_agent \\ "/programmingbitcoin:0.1/",
         latest_block \\ 0,
         relay \\ false
       ) do
@@ -51,7 +53,7 @@ defmodule VersionMessage do
         timestamp
       end
 
-    nonce =
+    message_nonce =
       if nonce == nil do
         Helpers.random_nonce()
       else
@@ -61,14 +63,14 @@ defmodule VersionMessage do
     %VersionMessage{
       version: version,
       services: services,
-      timestamp: timestamp,
+      timestamp: message_timestamp,
       receiver_services: receiver_services,
       receiver_ip: receiver_ip,
       receiver_port: receiver_port,
       sender_services: sender_services,
       sender_ip: sender_ip,
       sender_port: sender_port,
-      nonce: nonce,
+      nonce: message_nonce,
       user_agent: user_agent,
       latest_block: latest_block,
       relay: relay
@@ -91,15 +93,31 @@ defmodule VersionMessage do
         relay: relay
       }) do
     protocol_version = MathUtils.int_to_little_endian(version, 4)
-    network_services_sender = MathUtils.int_to_little_endian(sender_services, 8)
+    network_services = MathUtils.int_to_little_endian(services, 8)
     ser_timestamp = MathUtils.int_to_little_endian(timestamp, 8)
+    ser_result = protocol_version <> network_services <> ser_timestamp
+    # receiver
     network_services_receiver = MathUtils.int_to_little_endian(receiver_services, 8)
-    # is little endian
-    receiver_address = :binary.decode_unsigned(sender_ip)
-    ser_receiver_port = :binary.decode_unsigned(receiver_port)
-    ser_services_sender_2 = MathUtils.int_to_little_endian(sender_services, 8)
-    ser_sender_address = :binary.decode_unsigned(sender_ip)
-    ser_nonce = :binary.decode_unsigned(nonce)
-    #    ser_user_agent =
+    ip_v4_receiver_address = Helpers.ip_v4(receiver_ip)
+    ser_receiver_port = <<receiver_port::unsigned-big-integer-16>>
+
+    ser_result =
+      ser_result <> network_services_receiver <> ip_v4_receiver_address <> ser_receiver_port
+
+    # sender
+    ser_services_sender = MathUtils.int_to_little_endian(sender_services, 8)
+    ip_v4_sender_address = Helpers.ip_v4(sender_ip)
+    ser_sender_port = <<sender_port::unsigned-big-integer-16>>
+    ser_result = ser_result <> ser_services_sender <> ip_v4_sender_address <> ser_sender_port
+    # nonce + UserAgent
+    ser_result = ser_result <> nonce <> Tx.encode_varint(byte_size(user_agent)) <> user_agent
+    # Latest block is 4 bytes
+    ser_result = ser_result <> MathUtils.int_to_little_endian(latest_block, 4)
+
+    if relay do
+      ser_result <> <<0x01>>
+    else
+      ser_result <> <<0x00>>
+    end
   end
 end
---
+++ test/network/version_message_test.exs
@@ -0,0 +1,27 @@
+defmodule VersionMessageTest do
+  use ExUnit.Case
+
+  @tag :in_progress
+  test "serialize" do
+    nonce = :binary.copy(<<0x00>>, 8)
+
+    want =
+      "7f11010000000000000000000000000000000000000000000000000000000000000000000000ffff00000000208d000000000000000000000000000000000000ffff00000000208d0000000000000000182f70726f6772616d6d696e67626974636f696e3a302e312f0000000000"
+
+    ver_msg =
+      VersionMessage.new(
+        70015,
+        0,
+        0,
+        0,
+        <<0x00, 0x00, 0x00, 0x00>>,
+        8333,
+        0,
+        <<0x00, 0x00, 0x00, 0x00>>,
+        8333,
+        nonce
+      )
+
+    assert want == Base.encode16(VersionMessage.serialize(ver_msg), case: :lower)
+  end
+end
